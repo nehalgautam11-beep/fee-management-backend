7465ac19b25c1bbafa3244812daa7fc969b90f31
nehalgautam11-beep <nehalgautam11@gmail.com>
2026-02-11 20:21:45 +0530

    fix api url

--- school-erp/src/services/api.js
+++ school-erp/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 //import { logout } from "../hooks/useAuth"
 
 const API = axios.create({
-  baseURL: "http://localhost:3001", // ✅ BACKEND PORT
+  baseURL: process.env.REACT_APP_API_URL, // ✅ BACKEND PORT
   timeout: 10000
 })
 
